ce23fe5b0709c955f837d728c4204009d968af45
Alberto Tontoni <tontonialberto@outlook.it>
2026-06-05 18:22:34 +0200

    Refactor scope provider

--- packages/language/src/bean-scoping.ts
+++ packages/language/src/bean-scoping.ts
@@ -1,5 +1,5 @@
 import { AstNodeDescription, AstNodeDescriptionProvider, EMPTY_SCOPE, LangiumCoreServices, MapScope, ReferenceInfo, Scope, ScopeProvider } from "langium";
-import { Body, Case, Expression, isBody, isCase, isLetBinding, isTensorDestructor, LetBinding, TensorDestructor } from "./generated/ast.js";
+import { Body, Case, Expression, IdentifierDecl, isBody, isCase, isLetBinding, isTensorDestructor, TensorDestructor } from "./generated/ast.js";
 
 export class BeanScopeProvider implements ScopeProvider {
     private astNodeDescriptionProvider: AstNodeDescriptionProvider;
@@ -8,50 +8,34 @@ export class BeanScopeProvider implements ScopeProvider {
         this.astNodeDescriptionProvider = services.workspace.AstNodeDescriptionProvider;
     }
 
-    getScope(context: ReferenceInfo): Scope {
-        const container = context.container;
-
-        if (!container) return EMPTY_SCOPE;
+    getScope(ref: ReferenceInfo): Scope {
+        if (!ref.container) return EMPTY_SCOPE;
 
         let scope = EMPTY_SCOPE;
-
-        // Traverse up to find if this node is in a sequence
-        let current = container;
+        let current = ref.container;
         while (current && current.$container) {
             const container = current.$container;
 
-            // Check if current is in Body.content sequence
             if (isBody(container)) {
                 const body = container as Body;
-                const index = body.content.indexOf(current as Expression);
-                const previousVariablesDescriptions = 
-                    index >= 0 
-                        ? this.collectSequenceDeclarations(body.content, index)
-                        : [];
+                const indexInBody = body.content.indexOf(current as Expression);
                 const descriptions = [
-                    ...body.linearVarDecls.map(v =>
-                        this.astNodeDescriptionProvider.createDescription(v.ident, v.ident.name)
-                    ),
-                    ...body.discreteVarDecls.map(v =>
-                        this.astNodeDescriptionProvider.createDescription(v.ident, v.ident.name)
-                    ),
-                    ...previousVariablesDescriptions,
+                    ...this.collectContextDeclarations(body),
+                    ...this.collectSequenceDeclarations(body.content, indexInBody, ref),
                 ];
                 scope = new MapScope(descriptions, scope);
             }
-
-            // Check if current is in Case.leftBranch or Case.rightBranch sequence
-            if (isCase(container)) {
+            else if (isCase(container)) {
                 const caseNode = container as Case;
-                
+
                 const leftIndex = caseNode.leftBranch.indexOf(current as Expression);
                 if (leftIndex >= 0) {
-                    scope = new MapScope(this.collectCaseDeclarations(caseNode, leftIndex, true), scope);
+                    scope = new MapScope(this.collectCaseDeclarations(caseNode.leftName, caseNode.leftBranch, leftIndex, ref), scope);
                 }
-                
+
                 const rightIndex = caseNode.rightBranch.indexOf(current as Expression);
                 if (rightIndex >= 0) {
-                    scope = new MapScope(this.collectCaseDeclarations(caseNode, rightIndex, false), scope);
+                    scope = new MapScope(this.collectCaseDeclarations(caseNode.rightName, caseNode.rightBranch, rightIndex, ref), scope);
                 }
             }
 
@@ -62,67 +46,70 @@ export class BeanScopeProvider implements ScopeProvider {
     }
 
     /**
-     * Collect declarations from expressions in a sequence up to a given index
+     * 
+     * @param body 
+     * @returns Scoping information representing the variables declared in the body context.
      */
-    private collectSequenceDeclarations(sequence: Expression[], upToIndex: number): AstNodeDescription[] {
+    private collectContextDeclarations(body: Body): AstNodeDescription[] {
+        return [
+            ...body.linearVarDecls.map(v =>
+                this.astNodeDescriptionProvider.createDescription(v.ident, v.ident.name)
+            ),
+            ...body.discreteVarDecls.map(v =>
+                this.astNodeDescriptionProvider.createDescription(v.ident, v.ident.name)
+            ),
+        ];
+    }
+
+    /**
+     * @param sequence A sequence of expressions.
+     * @param upToIndex A non-negative integer.
+     * @param ref Information about the reference for which a scope is requested.
+     * @returns Scoping information about the given expressions.
+     */
+    private collectSequenceDeclarations(sequence: Expression[], upToIndex: number, ref: ReferenceInfo): AstNodeDescription[] {
         const descriptions: AstNodeDescription[] = [];
 
-        // Collect all declarations from expressions up to and including upToIndex
         for (let i = 0; i <= upToIndex; i++) {
-            descriptions.push(...this.extractDeclarationsFromExpression(sequence[i]));
+            descriptions.push(...this.collectExpressionDeclarations(sequence[i], ref));
         }
 
         return descriptions;
     }
 
     /**
-     * Collect declarations from Case node (shared by both branches)
+     * @param expr An `Expression` node.
+     * @param ref Information about the reference for which a scope is requested.
+     * @returns Scoping information representing the identifiers declared in the given expression.
      */
-    private collectCaseDeclarations(caseNode: Case, upToIndex: number, isLeftBranch: boolean): AstNodeDescription[] {
-        const descriptions: AstNodeDescription[] = [];
-
-        // Add the branch variable introduced by this case
-        if (isLeftBranch) {
-            descriptions.push(
-                this.astNodeDescriptionProvider.createDescription(caseNode.leftName, caseNode.leftName.name)
-            );
-            // Collect from left branch up to current point
-            for (let i = 0; i <= upToIndex; i++) {
-                descriptions.push(...this.extractDeclarationsFromExpression(caseNode.leftBranch[i]));
-            }
-        } else {
-            descriptions.push(
-                this.astNodeDescriptionProvider.createDescription(caseNode.rightName, caseNode.rightName.name)
-            );
-            // Collect from right branch up to current point
-            for (let i = 0; i <= upToIndex; i++) {
-                descriptions.push(...this.extractDeclarationsFromExpression(caseNode.rightBranch[i]));
-            }
-        }
-
-        return descriptions;
-    }
-
-    /**
-     * Extract variable declarations introduced by an expression
-     */
-    private extractDeclarationsFromExpression(expr: Expression): AstNodeDescription[] {
+    private collectExpressionDeclarations(expr: Expression, ref: ReferenceInfo): AstNodeDescription[] {
         if (isLetBinding(expr)) {
-            const letBinding = expr as LetBinding;
+            const ident = expr.name as IdentifierDecl;
             return [
-                this.astNodeDescriptionProvider.createDescription(letBinding.name, letBinding.name.name)
+                this.astNodeDescriptionProvider.createDescription(ident, ident.name)
             ];
         }
-
-        if (isTensorDestructor(expr)) {
+        else if (isTensorDestructor(expr)) {
             const tensorDest = expr as TensorDestructor;
             return [
                 this.astNodeDescriptionProvider.createDescription(tensorDest.id1, tensorDest.id1.name),
                 this.astNodeDescriptionProvider.createDescription(tensorDest.id2, tensorDest.id2.name)
             ];
         }
+        else return [];
+    }
 
-        // Case expressions don't declare variables in the sequence; their variables are only visible within branches
-        return [];
+    /**
+     * @param branchVar Identifier declared at the beginning of a `Case` branch.
+     * @param branch A sequence of expressions.
+     * @param upToIndex A non-negative integer.
+     * @param ref Information about the reference for which a scope is requested.
+     * @returns Scoping information about the given `Case` branch.
+     */
+    private collectCaseDeclarations(branchVar: IdentifierDecl, branch: Expression[], upToIndex: number, ref: ReferenceInfo): AstNodeDescription[] {
+        return [
+            this.astNodeDescriptionProvider.createDescription(branchVar, branchVar.name),
+            ...this.collectSequenceDeclarations(branch, upToIndex, ref),
+        ];
     }
 }
