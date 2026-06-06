b7c90a4d6b3c945420ec2d89b11f57f5b43355db
Alberto Tontoni <tontonialberto@outlook.it>
2026-06-06 16:16:17 +0200

    Refactor and polish diagnostic messages for interactive tutorial

--- packages/language/src/bean.lsp.ts
+++ packages/language/src/bean.lsp.ts
@@ -22,27 +22,6 @@ export class BeanCodeActionProvider implements CodeActionProvider {
     }
 
     /**
-     * Finds the AST node of type T overlapping with a given range in the document.
-     * @param document Langium document to be searched.
-     * @param range Range which the node should overlap with.
-     * @param typeGuard Desired node type.
-     * @returns The node in the given range, or otherwise if no such node exists.
-     */
-    private findNodeByRange<T = AstNode>(document: LangiumDocument, range: Range, typeGuard: (node: unknown) => node is T): T | undefined {
-        const root = document.parseResult.value;
-        const nodes = AstUtils.streamAst(root).filter(node => typeGuard(node));
-        const matchingNode = nodes.find(ident => this.rangesOverlap(ident.$cstNode!.range, range));
-        return matchingNode;
-    }
-
-    private rangesOverlap(range1: Range, range2: Range): boolean {
-        return !(range1.end.line < range2.start.line ||
-            range1.start.line > range2.end.line ||
-            (range1.end.line === range2.start.line && range1.end.character < range2.start.character) ||
-            (range1.start.line === range2.end.line && range1.start.character > range2.end.character));
-    }
-
-    /**
      * Creates a Code Action to remove the declaration of the given identifier node. 
      */
     private createRemoveDeclarationAction(
@@ -67,4 +46,24 @@ export class BeanCodeActionProvider implements CodeActionProvider {
         );
     }
 
+    /**
+     * Finds the AST node of type T overlapping with a given range in the document.
+     * @param document Langium document to be searched.
+     * @param range Range which the node should overlap with.
+     * @param typeGuard Desired node type.
+     * @returns The node in the given range, or otherwise if no such node exists.
+     */
+    private findNodeByRange<T = AstNode>(document: LangiumDocument, range: Range, typeGuard: (node: unknown) => node is T): T | undefined {
+        const root = document.parseResult.value;
+        const nodes = AstUtils.streamAst(root).filter(node => typeGuard(node));
+        const matchingNode = nodes.find(ident => this.rangesOverlap(ident.$cstNode!.range, range));
+        return matchingNode;
+    }
+
+    private rangesOverlap(range1: Range, range2: Range): boolean {
+        return !(range1.end.line < range2.start.line ||
+            range1.start.line > range2.end.line ||
+            (range1.end.line === range2.start.line && range1.end.character < range2.start.character) ||
+            (range1.start.line === range2.end.line && range1.start.character > range2.end.character));
+    }
 }
